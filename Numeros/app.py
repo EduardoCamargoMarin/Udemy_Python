1bcf8cac0775d9cc72101808aac043849ba6d00a
Eduardo Marin <du.marin10@gmail.com>
2023-01-06 22:14:47 -0300

    Udemy_Aulas

---
+++ Numeros/app.py
@@ -0,0 +1,21 @@
+print(6)
+print(-20)
+print(8.39049)
+print(4+8*2/29)
+print(4+8+(2/29))
+
+
+
+num1 = 1
+num2 = 2
+numero_sorte = 5
+
+print(str(numero_sorte) + ' é o meu numero da sorte') #para funcionar precisa mostrar que o numero é uma string
+# precisa fazer a conversão 
+print(num1 + num2)
+
+
+print(min(2, 8)) #traz o menor valor entre as opções - para ter o maior .. usar o max
+print(pow(2,3)) #Pow quer dizer 'elevado a '
+
+print(round(11.7)) #arredonda
